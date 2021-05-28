c77754475f1347d947faf8050d3c0492caf8c9c9
Flavio <flavio.origgi@outlook.it>
2021-05-28 14:32:56 +0200

    generazione due numeri casuali

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia 
+// per il computer. Stabilire il vincitore, in base a chi fa il 
+// punteggio più alto.
+
+// 1 generare 2 numeri random 
+var utente = Math.floor(Math.random()*5+1);
+var pc = Math.floor(Math.random()*5+1);
